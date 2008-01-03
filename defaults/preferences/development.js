3938a9ad8d8ca4c668f4bb8644ccb93bd5229f55
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2008-01-03 21:02:37 +0000

    Monotone-Parent: fd18a53166c18e77bca592ba10c224a0e4339898 Monotone-Revision: ddb3efe8c3147f3140e26bcb59d3a571c69f09ab
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2008-01-03T21:02:37

--- defaults/preferences/development.js
+++
@@ -1,7 +0,0 @@
-if (0) {
-  pref("javascript.options.showInConsole", true);
-  pref("nglayout.debug.disable_xul_cache", true);
-  pref("browser.dom.window.dump.enabled", true);
-  pref("javascript.options.strict", true);
-}
-
