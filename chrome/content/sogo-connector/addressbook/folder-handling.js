7b99d606d8cc9137087bcfc3849511fbbd601a6e
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2010-06-25 01:51:32 +0000

    Monotone-Parent: 23bdf385b10cae3e51aef3ccc0838b0b7d506a2c Monotone-Revision: f7f56b5e9c41944afefa33b540121a6a9c286de0
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2010-06-25T01:51:32

--- chrome/content/sogo-connector/addressbook/folder-handling.js
+++ chrome/content/sogo-connector/addressbook/folder-handling.js
@@ -20,9 +20,9 @@
  */
 
 function jsInclude(files, target) {
-    var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+    let loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
                            .getService(Components.interfaces.mozIJSSubScriptLoader);
-    for (var i = 0; i < files.length; i++) {
+    for (let i = 0; i < files.length; i++) {
         try {
             loader.loadSubScript(files[i], target);
         }
@@ -39,112 +39,76 @@ function jsInclude(files, target) {
 
 jsInclude(["chrome://sogo-connector/content/general/preference.service.addressbook.groupdav.js"]);
 
-/* We MUST enumerate the directories to find the current directory. Otherwise,
- if we use the GetResource method, the top directory will not recognize the
- CardDAV entries with the hasDirectory method. */
 function SCGetDirectoryFromURI(uri) {
-    var directory = null;
-
-    // 	dump("getting dir: " + uri + "\n");
-    if (uri && uri.length > 0) {
-        var uriParts = uri.split("/");
-        var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"]
-                            .getService(Components.interfaces.nsIRDFService);
-        var parentDir = rdf.GetResource("moz-abdirectory://")
-                           .QueryInterface(Components.interfaces.nsIAbDirectory);
-        var currentURI = uriParts[0] + "/";
-        var currentDirectory = parentDir;
-        for (var i = 2; currentDirectory && i < uriParts.length; i++) {
-            currentURI += "/" + uriParts[i];
-            // 			dump("currentURI:  " + currentURI + "\n");
-            currentDirectory = _SCGetChildDirectoryFromURI(currentDirectory,
-                                                           currentURI);
-        }
-        directory = currentDirectory;
-    }
-    else {
-        dump("wrong uri: " + uri + "\n");
-        if (uri)
-            dump("possible ab is: " + uri.Value + "\n");
-
-        dump("backtrace: " + backtrace() + "\n");
-        directory = null;
-    }
-
-    // 	if (directory)
-    // 		dump("dir found!\n");
-
-    return directory;
-}
-
-function _SCGetChildDirectoryFromURI(parentDir, uri) {
-    var directory = null;
-
-    var nodes = parentDir.childNodes;
-    while (!directory && nodes.hasMoreElements()) {
-        var currentChild = nodes.getNext()
-                                .QueryInterface(Components.interfaces.nsIRDFResource);
-        if (currentChild.Value == uri)
-            directory = currentChild.QueryInterface(Components.interfaces.nsIAbDirectory);
-    }
-
-    return directory;
+    // dump("SCGetDirectoryFromURI: " + uri + "\n");
+    let abManager = Components.classes["@mozilla.org/abmanager;1"]
+                              .getService(Components.interfaces.nsIAbManager);
+    return abManager.getDirectory(uri);
 }
 
 function SCCreateCardDAVDirectory(description, url) {
-    var properties = Components.classes["@mozilla.org/addressbook/properties;1"]
-                               .createInstance(Components.interfaces.nsIAbDirectoryProperties);
-    properties.dirType = 0;
-    properties.description = description;
-    properties.URI = "carddav://" + url;
-    properties.maxHits = 10; // TODO
+    let abMgr = Components.classes["@mozilla.org/abmanager;1"]
+                          .getService(Components.interfaces.nsIAbManager);
+    let prefId = abMgr.newAddressBook(description, "carddav://" + url, 0);
 
-    var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"]
-                        .getService(Components.interfaces.nsIRDFService);
-    var parentDir = rdf.GetResource("moz-abdirectory://")
-                       .QueryInterface(Components.interfaces.nsIAbDirectory);
-    parentDir.createNewDirectory(properties);
-
-    return SCGetDirectoryFromURI("moz-abdavdirectory://" + properties.prefName);
+    return SCGetDirectoryFromURI("moz-abdavdirectory://" + prefId);
 }
 
 function SCCreateGroupDAVDirectory(description, url) {
-    var properties = Components.classes["@mozilla.org/addressbook/properties;1"]
-                               .createInstance(Components.interfaces.nsIAbDirectoryProperties);
-    properties.dirType = 2;
-    properties.description = description;
-
-    var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"]
-                        .getService(Components.interfaces.nsIRDFService);
-    var parentDir = rdf.GetResource("moz-abdirectory://")
-                       .QueryInterface(Components.interfaces.nsIAbDirectory);
-    parentDir.createNewDirectory(properties);
-
-    var groupdavPrefService = new GroupdavPreferenceService(properties.prefName);
+    let abMgr = Components.classes["@mozilla.org/abmanager;1"]
+                          .getService(Components.interfaces.nsIAbManager);
+    let prefId = abMgr.newAddressBook(description, null,
+                                      2 /* don't know which values should go in
+                                         there but 2 seems to get the job
+                                         done */);
+    let groupdavPrefService = new GroupdavPreferenceService(prefId);
     groupdavPrefService.setURL(url);
 
-    return SCGetDirectoryFromURI("moz-abmdbdirectory://" + properties.fileName);
+    let prefService = Components.classes["@mozilla.org/preferences-service;1"]
+                                .getService(Components.interfaces.nsIPrefBranch);
+    let filename = prefService.getCharPref(prefId + ".filename");
+
+    return SCGetDirectoryFromURI("moz-abmdbdirectory://" + filename);
 }
 
 function SCDeleteDirectoryWithURI(uri) {
-    var directory = SCGetDirectoryFromURI(uri);
+    let directory = SCGetDirectoryFromURI(uri);
     if (directory)
         SCDeleteDirectory(directory);
 }
 
 function SCDeleteDirectory(directory) {
-    directory = directory.QueryInterface(Components.interfaces.nsIAbDirectory);
-    var prefBranch = directory.dirPrefId;
+    dump("SCDeleteDirectory: "  + directory + "\n");
 
-    _SCDeleteAddressBook(directory);
-    var prefService = Components.classes["@mozilla.org/preferences-service;1"]
+    /* We need to use the nsIRDFResource interface here as identifier because
+     the value of .URI returns the "carddav://http..." url on CardDAV dirs.
+     Note that this problem does not happen with MDB/GroupDAV dirs, where
+     the .URI and the .Value props have the same apparent value. */
+    let rdfAB = directory.QueryInterface(Components.interfaces.nsIRDFResource);
+    let rdfValue = rdfAB.Value;
+    dump("   delete rdfValue: " + rdfValue + "\n" );
+
+    let abMgr = Components.classes["@mozilla.org/abmanager;1"]
+                          .getService(Components.interfaces.nsIAbManager);
+    try {
+        abMgr.deleteAddressBook(rdfValue);
+    }
+    catch(e) {
+        dump("folder-handling.js: failed to delete '" + rdfValue + "'\n" + e);
+        if (e.fileName)
+            dump ("\nFile: " + e.fileName
+                  + "\nLine: " + e.lineNumber
+                  + "\n\n Stack:\n\n" + e.stack);
+    }
+
+    let prefService = Components.classes["@mozilla.org/preferences-service;1"]
                                 .getService(Components.interfaces.nsIPrefBranch);
+    let prefBranch = directory.dirPrefId;
+    dump("  dirPrefId: "  + prefBranch + "\n");
     prefService.deleteBranch(prefBranch + ".position");
 
-    var rdfDirectory
-        = directory.QueryInterface(Components.interfaces.nsIRDFResource);
-    var clearPrefsRequired
-        = (prefService.getCharPref("mail.collect_addressbook") == rdfDirectory.Value
+    let clearPrefsRequired
+        = (prefService.getCharPref("mail.collect_addressbook") == rdfValue
            && (prefService.getBoolPref("mail.collect_email_address_outgoing")
                || prefService.getBoolPref("mail.collect_email_address_incoming")
                || prefService.getBoolPref("mail.collect_email_address_newsgroup")));
@@ -155,51 +119,27 @@ function SCDeleteDirectory(directory) {
         prefService.setBoolPref("mail.collect_email_address_newsgroup", false);
         prefService.setCharPref("mail.collect_addressbook",	"moz-abmdbdirectory://abook.mab");
     }
-}
 
-function _SCDeleteAddressBook(directory) {
-    var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"]
-                        .getService(Components.interfaces.nsIRDFService);
-    var parentDir = rdf.GetResource("moz-abdirectory://")
-                       .QueryInterface(Components.interfaces.nsIAbDirectory);
-
-    // 	dump("dir: " + directory + "\n");
-    // 	dump("dir.dirName: " + directory.dirName + "\n");
-    // 	dump("backtrace: " + backtrace() + "\n");
-    parentDir.deleteDirectory(directory);
-    // 	var ab = Components.classes["@mozilla.org/addressbook;1"]
-    // 		.createInstance(Components.interfaces.nsIAddressBook);
-
-    // 	var parentArray = Components.classes["@mozilla.org/supports-array;1"]
-    // 		.createInstance(Components.interfaces.nsISupportsArray);
-    // 	parentArray.AppendElement(parentDir);
-    // 	var abArray = Components.classes["@mozilla.org/supports-array;1"]
-    // 		.createInstance(Components.interfaces.nsISupportsArray);
-    // 	parentArray.AppendElement(directory);
-
-    // 	dump("deleteaddressbook...\n");
-    // 	var ds = Components
-    // 		.classes["@mozilla.org/rdf/datasource;1?name=addressdirectory"]
-    // 		.getService(Components.interfaces.nsIRDFDataSource);
-    // 	ab.deleteAddressBooks(ds, parentArray, abArray);
-    // 	dump("delete done\n");
+    dump("  deleted done\n");
 }
 
 function SCDeleteDirectories(directories) {
-    for (var i = 0; i < directories.length; i++)
+    for (let i = 0; i < directories.length; i++) {
         SCDeleteDirectory(directories[i]);
+    }
 }
 
 function SCDeleteDAVDirectory(uri) {
-    var result = false;
+    let result = false;
+    dump("SCDeleteDAVDirectory : " + uri + "\n");
 
     if (isGroupdavDirectory(uri) || isCardDavDirectory(uri)) {
-        var directory = SCGetDirectoryFromURI(uri);
+        let directory = SCGetDirectoryFromURI(uri);
         if (directory) {
             try {
                 SCDeleteDirectory(directory);
-                var prefBranch = directory.dirPrefId;
-                var prefService = Components.classes["@mozilla.org/preferences-service;1"]
+                let prefBranch = directory.dirPrefId;
+                let prefService = Components.classes["@mozilla.org/preferences-service;1"]
                                             .getService(Components.interfaces.nsIPrefBranch);
                 /* groupdav = moz-abmdbdirectory, carddav = moz-abdavdirectory */
                 if (uri.indexOf("moz-abmdbdirectory://") == 0)
@@ -208,54 +148,43 @@ function SCDeleteDAVDirectory(uri) {
 
                 result = true;
             }
-            catch(e) {};
+            catch(e) {
+                dump("folder-handling.js: failed to delete '" + uri + "'\n" + e);
+                if (e.fileName)
+                    dump ("\nFile: " + e.fileName
+                          + "\nLine: " + e.lineNumber
+                          + "\n\n Stack:\n\n" + e.stack);
+            };
         }
     }
-    else
+    else {
+        dump("attempting to delete a non-DAV directory: " + uri
+             + "\n" + backtrace() + "\n\n");
         throw("attempting to delete a non-DAV directory: " + uri);
+    }
 
     return result;
 }
 
-function SCGetChildCards(directory) {
-    var childCards = [];
-
-    var rdf = Components.classes["@mozilla.org/rdf/rdf-service;1"]
-                        .getService(Components.interfaces.nsIRDFService);
-    var ds = Components.classes["@mozilla.org/rdf/datasource;1?name=addressdirectory"]
-                       .getService(Components.interfaces.nsIRDFDataSource);
-    var childSrc = rdf.GetResource("http://home.netscape.com/NC-rdf#CardChild");
-    var cards = ds.GetTargets(directory, childSrc, false);
-    while (cards.hasMoreElements()) {
-        var card = cards.getNext().QueryInterface(Components.interfaces.nsIAbCard);
-        if (!card.isMailList)
-            childCards.push(card);
-    }
-
-    dump("got " + childCards.length + " cards\n");
-
-    return childCards;
-}
-
 function SCCopyAddressBook(sourceAB, destAB) {
-    destAB = destAB.QueryInterface(Components.interfaces.nsIAbDirectory);
-    var childCards = SCGetChildCards(sourceAB);
-    for (var i = 0; i < childCards.length; i++)
-        destAB.addCard(childCards[i]);
-
-    var nodes = sourceAB.childNodes;
-    while (nodes.hasMoreElements()) {
-        var currentList = nodes.getNext()
-                               .QueryInterface(Components.interfaces.nsIAbDirectory);
-        var newList = Components.classes["@mozilla.org/addressbook/directoryproperty;1"]
-                                .createInstance(Components.interfaces.nsIAbDirectory);
-        newList.dirName = currentList.dirName;
-        newList.listNickName = currentList.listNickName;
-        newList.description = currentList.description;
-
-        childCards = SCGetChildCards(currentList);
-        for (i = 0; i < childCards.length; i++)
-            newList.addressLists.AppendElement(childCards[i]);
-        destAB.addMailList(newList);
+    let childCards = sourceAB.childCards;
+    while (childCards.hasMoreElements()) {
+        destAB.addCard(childCards.getNext());
     }
+
+    // let nodes = sourceAB.childNodes;
+    // while (nodes.hasMoreElements()) {
+    //     let currentList = nodes.getNext()
+    //                            .QueryInterface(Components.interfaces.nsIAbDirectory);
+    //     let newList = Components.classes["@mozilla.org/addressbook/directoryproperty;1"]
+    //                             .createInstance(Components.interfaces.nsIAbDirectory);
+    //     newList.dirName = currentList.dirName;
+    //     newList.listNickName = currentList.listNickName;
+    //     newList.description = currentList.description;
+
+    //     let childCards = SCGetChildCards(currentList);
+    //     for (let i = 0; i < childCards.length; i++)
+    //         newList.addressLists.AppendElement(childCards[i]);
+    //     destAB.addMailList(newList);
+    // }
 }
